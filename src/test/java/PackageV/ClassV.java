f4a4edde7d5799bda291f1a1b904d9028b912cfd
Singh <Sandeep.Singh@qualitest.local>
2022-09-27 23:40:49 +0530

    Sample Commit 01 0927

---
+++ src/test/java/PackageV/ClassV.java
@@ -0,0 +1,11 @@
+package PackageV;
+
+public class ClassV {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Sample code Push V1");
+System.out.println();
+	}
+
+}
